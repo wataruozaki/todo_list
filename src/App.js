496e8a29c9846e2d05444e1022d6b6e65368fcaa
wataru <dossjakapoko@gmail.com>
2020-07-02 18:31:11 +0900

    start

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useReducer } from "react";
+import { Grid, TextField, Button, Paper, makeStyles } from "@material-ui/core";
 
+const useStyles = makeStyles(() => ({
+  paper: {
+    maxWidth: "500px",
+    margin: "50px auto",
+    padding: "50px",
+    textAlign: "center",
+  },
+  textField: {
+    width: "100%",
+  },
+  button: {
+    width: "100%",
+  },
+}));
 function App() {
+  const classes = useStyles();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form>
+      <Paper className={classes.paper}>
+        <h1>Todo List</h1>
+        <Grid container spacing={3}>
+          <Grid item xs={4}>
+            <TextField className={classes.textField} helperText="title" />
+          </Grid>
+          <Grid item xs={8}>
+            <TextField className={classes.textField} helperText="body" />
+          </Grid>
+          <Grid item xs={6}>
+            <Button className={classes.button} variant="contained">
+              Add
+            </Button>
+          </Grid>
+          <Grid item xs={6}>
+            <Button className={classes.button} variant="contained">
+              All Delete
+            </Button>
+          </Grid>
+        </Grid>
+      </Paper>
+    </form>
   );
 }
 
